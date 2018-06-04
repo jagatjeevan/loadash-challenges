f1996c1d9090a4e1479cc146305e1ec785d76599
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2018-06-04 11:42:39 +0530

    Added flow to manage type of the arguments

--- src/concat.js
+++ src/concat.js
@@ -1,4 +1,9 @@
-const concat = (arr) => arr;
-  // TODO : the functionality
+// @flow
+
+const concat = (...arr: Array<mixed>): Array<mixed> => {
+  const resultArr = [];
+  arr.forEach(elem => resultArr.push(elem));
+  return resultArr;
+};
 
 export default concat;
--- test/concat.test.js
+++ test/concat.test.js
@@ -1,8 +1,15 @@
 import concat from '../src/concat';
 
-const arr = [1,2,3];
-const expected = [2,3,4];
+test('concats element to an empty array', () => {
+  const emptyArr = [];
+  expect(emptyArr.concat(4)).toEqual([4]);
+  expect(emptyArr.concat('a')).toEqual(['a']);
+  expect(emptyArr.concat('a', 'b', 5)).toEqual(['a', 'b', 5]);
+});
 
-test('returns the array', () => {
-  expect(concat(arr)).toEqual(arr);
+test('concats element to a non-empty array', () => {
+  const arr = [1, 2];
+  expect(arr.concat(4)).toEqual([1, 2, 4]);
+  expect(arr.concat('a')).toEqual([1, 2, 'a']);
+  expect(arr.concat('a', 'b', 5)).toEqual([1, 2, 'a', 'b', 5]);
 });
